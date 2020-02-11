6d5ad9bca3747017f7c12b6035ce72a746584e93
Leon Schlömmer <leon.schloemmer@posteo.at>
2020-02-11 09:32:07 +0100

    Add Project

---
+++ src/test/java/at/htl/boundary/NativeHelloSourceIT.java
@@ -0,0 +1,9 @@
+package at.htl.boundary;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloSourceIT extends HelloSourceTest {
+
+    // Execute the same tests but in native mode.
+}
